b78cdd60c27a3faafd49d493f034504d8a64684b
saadmunaf123 <saadmunafbangi123@gmail.com>
2026-06-21 17:01:07 +0530

    v2.9

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ const app = express();
 
 app.use(cors({
   origin: [
-    // 'http://localhost:3000',
-    'https://lms-backend-9jj7.onrender.com'
+    'http://localhost:3000',
+    'https://lms-mca.vercel.app'
   ],
   credentials: true,
 }));
